7bb135bd8bf7dd552a4e82eda3dbab4761eb4c97
EatFan <eatfan0921@163.com>
2025-08-07 10:18:11 +0800

    feat: 封装注册接口

---
+++ register_api.py
@@ -0,0 +1,25 @@
+# 注册接口API封装
+import requests
+
+def verifyEmailAddress(email):
+    """
+    验证邮箱地址接口
+    :param email: 邮箱
+    :return: 响应结果
+    """
+
+
+def activationEmail(email,key):
+    """
+    激活邮箱接口
+    :param email: 邮箱
+    :param key: 邮件中的key
+    :return: 响应结果
+    """
+
+def register(user):
+    """
+    注册接口
+    :param user: 用户
+    :return: 响应结果
+    """
